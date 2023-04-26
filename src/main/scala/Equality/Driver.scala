b3f714740587b9a880bb9b81d0246224106afcbb
Akhil1857 <akhil.trivedi@knoldus.com>
2023-04-26 22:47:59 +0530

    Driver

--- src/main/scala/Equality/Driver.scala
+++ src/main/scala/Equality/Driver.scala
@@ -1,14 +1,16 @@
 package Equality
 
+import scala.util.{Failure, Success}
+
 object Driver extends App {
 
   private val convertor = new Convertor
-  private val spelling = List("twenty hundred one", "Twelve", "Six")
+  private val spelling = List("one hundred one","twelve","six")
 
   //Function Call
   convertor.spellingsToNumbers(spelling.map(_.toLowerCase)) match {
-    case Some(value) => println(value) // prints List(NumberWordPair(0,(Error:-Value>100) Please Enter value <= 100), NumberWordPair(12,twelve), NumberWordPair(6,six))
-    case None => print("Error")
+    case Success(value) => println(value) // prints List(NumberWordPair(0,(Error:-Value>100) Please Enter value <= 100), NumberWordPair(12,twelve), NumberWordPair(6,six))
+    case Failure(ex) => println(s"Error+${ex.getMessage}")
   }
 
 }
