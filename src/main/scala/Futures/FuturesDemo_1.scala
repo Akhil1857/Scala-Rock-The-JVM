398b911aab1cbe075c2b6042e41373fa4874d366
Akhil1857 <akhil.trivedi@knoldus.com>
2023-04-26 18:56:32 +0530

    26-April-2023

--- src/main/scala/Futures/FuturesDemo_1.scala
+++ src/main/scala/Futures/FuturesDemo_1.scala
@@ -1,7 +1,8 @@
 package Futures
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success}
 
 object SocialNetwork extends App {
@@ -44,7 +45,7 @@ object SocialNetwork extends App {
     Profile(id, names(id))
   }
 
-  def fetchBestFriend(profile: Profile): Future[Profile] = Future {
+  private def fetchBestFriend(profile: Profile): Future[Profile] = Future {
     Thread.sleep(40)
     val bfId = friends(profile.id)
     Profile(bfId, names(bfId))
@@ -75,5 +76,52 @@ object SocialNetwork extends App {
   val billBestFriendRestricted = johnBestFriends.filter(profile => profile.name.startsWith("X"))
   Thread.sleep(1000)
   println(billBestFriendRestricted)
+
+  //fallbacks
+  val aProfileNoMatterwhat = SocialNetwork.fetchProfile(id = "unknown - id").recover {
+    case e: Throwable => Profile("fb.id.03-dumm","Forever Alone")
+  }
+  Thread.sleep(500)
+  println(aProfileNoMatterwhat)
+
+  val aFetchProfileNoMatterwhat = SocialNetwork.fetchProfile(id = "fb.id.02-bill").recoverWith {
+    case e: Throwable => Future(SocialNetwork.fetchProfile("fb.id.02-dummy"))
+  }
+  Thread.sleep(500)
+  println(aFetchProfileNoMatterwhat)
+
+  // online banking app
+
+  case class User(name:String)
+  case class Transaction(sender:String,receiver:String,amount:Double,status:String)
+
+  object BankingApp {
+    val name = "Banking"
+
+    def fetchUser(name:String):Future[User] = Future {
+      Thread.sleep(500)
+      User(name)
+    }
+    def createTransaction(user:User, merchantName: String,amount:Double):Future[Transaction] =Future {
+      Thread.sleep(1000)
+      Transaction(user.name,merchantName,amount,"Fails")
+    }
+
+    def purchase(username:String,item:String,merchantName:String,cost:Double):String = {
+      //fetch the user from the Database
+      //create a Transaction
+      //Wait for the transaction to finish
+
+      val transactiionStatus = for {
+        user <- fetchUser(username)
+        transaction <- createTransaction(user,merchantName,cost)
+      }yield transaction.status
+
+      Await.result(transactiionStatus,2.seconds)
+    }
+  }
+
+  println(BankingApp.purchase("John","Bag","SkyBags",3000))
+
 
 }
